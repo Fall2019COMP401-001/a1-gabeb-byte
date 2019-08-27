d2db3caf22692c28cef6f09039934f2dba870b37
gabebarfield <gabebarfield@gabes-air.wireless-1x.unc.edu>
2019-08-26 21:29:13 -0400

    Wrote for loops to go through each customer and their purchases.
    Added a space between loops.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,24 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Number of customers
+		int customers = scan.nextInt();
+		
+		
+		for(int i=0; i<customers; i++) {
+			double total = 0;
+			String first = scan.next();
+			String last = scan.next();
+			int numOfItems = scan.nextInt();
+			
+			for (int x=0; x<numOfItems; x++) {
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+				double itemPrice = scan.nextDouble();
+				total = total + (quantity*itemPrice);
+			}
+			System.out.println("" + first.charAt(0) + ". " + last + ": " + total);
+		}
+		scan.close();
 	}
 }
