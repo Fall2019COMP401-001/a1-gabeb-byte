ee1b19738a97b2ae54ed7c1d8661c7df31645af5
gabebarfield <gabebarfield@babadook.wireless-1x.unc.edu>
2019-09-02 00:00:53 -0400

    Implemented for loops to cycle through arrays tracking an item's name, how many of it were bought, and how many customers bought each.

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,57 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int storeItems = scan.nextInt();
+		String[] itemNames = new String[storeItems];
+		int[] numOfItem = new int[storeItems];
+		int[] numOfCustomers = new int[storeItems];
 		
+		for (int i=0; i<storeItems; i++) {
+			itemNames[i] = scan.next();
+			double price = scan.nextDouble();
+			
+		}
+		int customers = scan.nextInt();
+		for (int i=0; i<customers; i++) {
+			String custName = scan.next() + " " + scan.next();
+			int itemsBought = scan.nextInt();
+			int[] itemChange = new int[storeItems];
+			
+			
+			for (int x=0; x<itemsBought; x++) {
+				
+				int itemAmount = scan.nextInt();
+				String item = scan.next();
+				
+				
+				for (int y=0; y<itemNames.length; y++) {
+					
+					
+					if (itemNames[y].contentEquals(item)) {
+						numOfItem[y] += itemAmount;
+						itemChange[y]++;
+					
+						
+					} 
+					
+				}
+				
+				
+			}
+			for (int z=0; z<itemChange.length; z++) {
+				if (itemChange[z]>0) {
+					numOfCustomers[z]++;
+				}
+			}
+		}
+		for (int i=0; i<numOfItem.length; i++ ) {
+			if (numOfCustomers[i]==0) {
+				System.out.println("No customers bought " + itemNames[i]);
+			} else {
+				System.out.println(numOfCustomers[i] + " customers bought " + numOfItem[i] + " " + itemNames[i]);
+			}
+		}
+		
+		scan.close();
 	}
 }
